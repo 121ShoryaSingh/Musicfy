76803edf3d8db5a4945e152f111c57bd98018944
121ShoryaSingh <139441120+121ShoryaSingh@users.noreply.github.com>
2024-06-07 13:38:07 +0530

    basic_layout_complete

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,9 +9,15 @@ export default function Home() {
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-3 mt-4">
-          <ListItem image="/images/liked.png"/>
+          <ListItem image="/images/liked.png" name="Liked Songs" href="liked"/>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-while text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>List of Songs</div>
+      </div>
     </div>
   );
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -10,6 +10,7 @@ interface ListItemProps {
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import Image from 'next/image';
+import { FaPlay } from 'react-icons/fa';
 
 
 const ListItem: React.FC<ListItemProps> = ({ image, name, href}) => {
@@ -20,10 +21,12 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href}) => {
   }
 
   return (
-    <button className='relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4 '>
+    <button onClick={onClick} className='relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4 '>
       <div className='relative min-h-[64px] min-w-[64px]'>
-        <Image className="object-cover" fill src={image} alt={Image}/>
+        <Image className="object-cover" fill src={image} alt="Image"/>
       </div>
+      <p className='font-medium truncate py-5'>{name}</p>
+      <div className='text-black opacity-0 absolute transition rounded-full flex items-center justify-center bg-rose-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110'><FaPlay/></div>
     </button>
   )
 }
