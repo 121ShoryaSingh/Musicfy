2d4905e314ab63ff8b657f7bf81a8aa785b8c865
121ShoryaSingh <139441120+121ShoryaSingh@users.noreply.github.com>
2024-06-09 05:36:59 +0530

    custom hooks and types

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Figtree, Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
+import SupabaseProvider from "@/provider/SupabaseProvider";
+
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>
+            {children}
+          </Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
--- provider/SupabaseProvider.tsx
+++ provider/SupabaseProvider.tsx
@@ -7,11 +7,16 @@ interface SupabaseProviderProps {
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import React, { useState } from 'react'
 import { Database } from '@/types_db';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 export const SupabaseProvider: React.FC<SupabaseProviderProps> = ({children}) => {
     const [supabaseClient] = useState(() => createClientComponentClient<Database>())
   return (
     
-    <div></div>
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
   )
 }
+
+export default SupabaseProvider;
